ff2ac686bd7700d8fd80db1400077f340448c82b
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-04-10 14:18:01 +0200

    init ergebnismeldung from spring.io

---
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/ErgebnismeldungApplication.java
@@ -0,0 +1,13 @@
+package com.fb.dlqpattern.ergebnismeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgebnismeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErgebnismeldungApplication.class, args);
+	}
+
+}
