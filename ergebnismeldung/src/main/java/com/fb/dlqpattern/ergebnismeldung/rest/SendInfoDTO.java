ee919831b219f3b13e2cf30cba63c4c10587930c
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-05-13 13:24:02 +0200

    adjust record in SendInfoDTO to match properties in SendInfo.

--- ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/rest/SendInfoDTO.java
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/rest/SendInfoDTO.java
@@ -2,6 +2,6 @@ package com.fb.dlqpattern.ergebnismeldung.rest;
 
 import org.springframework.lang.NonNull;
 
-public record SendInfoDTO(@NonNull String wahllokalID, @NonNull String nachricht) {
+public record SendInfoDTO(@NonNull int id, @NonNull String name, @NonNull int age, @NonNull String kind) {
 
 }
