a97c6c39b0bd8aba43dda62985d4fad9835eb8c8
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-05-13 08:37:54 +0200

    added in SendInfo.java getter and Setter with lombok

--- ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/domain/SendInfo.java
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/domain/SendInfo.java
@@ -1,6 +1,11 @@
 package com.fb.dlqpattern.ergebnismeldung.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class SendInfo {
+    @Getter
+    @Setter
     private int id;
     private String name;
     private int age;
@@ -13,35 +18,5 @@ public class SendInfo {
         this.kind = kind;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
 }
