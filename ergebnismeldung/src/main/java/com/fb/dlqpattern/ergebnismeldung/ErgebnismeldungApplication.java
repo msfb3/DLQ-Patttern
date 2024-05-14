1ca636e8e806c560b1963363cda7fb4fce8641e8
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-05-14 09:42:20 +0200

    Removed unused imports in ErgebnismeldungApplication

--- ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/ErgebnismeldungApplication.java
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/ErgebnismeldungApplication.java
@@ -1,10 +1,8 @@
 package com.fb.dlqpattern.ergebnismeldung;
 
-import com.fb.dlqpattern.ergebnismeldung.rest.SendInfoDTO;
-import com.fb.dlqpattern.ergebnismeldung.service.SendErgebnismeldungService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
 public class ErgebnismeldungApplication {
