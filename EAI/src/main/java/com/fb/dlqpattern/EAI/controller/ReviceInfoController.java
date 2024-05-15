02a040c39841ac04e51021d7f371f7810f2b6f16
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-05-15 11:12:23 +0200

    added RestController annotation to class.

--- EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReviceInfoController.java
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReviceInfoController.java
@@ -3,6 +3,9 @@ package com.fb.dlqpattern.EAI.controller;
 import com.fb.dlqpattern.EAI.service.ReviceErgebnismeldungService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class ReviceInfoController {
 
