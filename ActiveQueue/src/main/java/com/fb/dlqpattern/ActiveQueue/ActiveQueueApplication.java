75d232a96a59403a32db6b0f2313652f6ed7e962
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-03 12:16:46 +0200

    Dateien ergänzt.

---
+++ ActiveQueue/src/main/java/com/fb/dlqpattern/ActiveQueue/ActiveQueueApplication.java
@@ -0,0 +1,13 @@
+package com.fb.dlqpattern.ActiveQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveQueueApplication.class, args);
+	}
+
+}
---
+++ ActiveQueue/src/test/java/com/fb/dlqpattern/ActiveQueue/ActiveQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.dlqpattern.ActiveQueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
