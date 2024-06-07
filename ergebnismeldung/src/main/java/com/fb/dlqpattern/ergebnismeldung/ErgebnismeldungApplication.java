03d6c782fb450e24f29c20a2761cd6f0fe023b97
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-06-07 09:43:22 +0200

    Werfe Fachliche WLS-Exception bei Unstimmigkeiten in der DTO GlobalExceptionHandler eingebunden wls.common.exception eingebunden

--- ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/ErgebnismeldungApplication.java
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/ErgebnismeldungApplication.java
@@ -1,10 +1,15 @@
 package com.fb.dlqpattern.ergebnismeldung;
 
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.rest.model.DTOMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.fb.dlqpattern.ergebnismeldung",
+		"de.muenchen.oss.wahllokalsystem"
+})
 public class ErgebnismeldungApplication {
 
 	public static void main(String[] args) {
--- ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/domain/SendInfo.java
+++ ergebnismeldung/src/main/java/com/fb/dlqpattern/ergebnismeldung/domain/SendInfo.java
@@ -1,22 +1,17 @@
 package com.fb.dlqpattern.ergebnismeldung.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SendInfo {
-    @Getter
-    @Setter
     private int id;
     private String name;
     private int age;
     private String kind;
-
-    public SendInfo( int id, String name, int age, String kind) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.kind = kind;
-    }
-
-
 }
