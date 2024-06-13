36588bfa81430ceac71937992c82fc9df5abb05d
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-13 14:21:22 +0200

    added PostMapping to Controller

--- was/src/main/java/com/fb/dlqpattern/was/controller/ReceivedDataController.java
+++ was/src/main/java/com/fb/dlqpattern/was/controller/ReceivedDataController.java
@@ -1,5 +1,10 @@
 package com.fb.dlqpattern.was.controller;
 
+import com.fb.dlqpattern.was.rest.ReceivedDataDTO;
+import com.fb.dlqpattern.was.service.ReceivedDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -7,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class ReceivedDataController {
 
+    @Autowired
+    ReceivedDataService receivedDataService;
 
+    @PostMapping("/was")
+    public void receiveData(@RequestBody ReceivedDataDTO receivedDataDTO) {
+        receivedDataService.receiveFromDTO(receivedDataDTO);
+    }
 }
