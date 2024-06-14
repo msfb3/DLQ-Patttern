76c8c31cbef9f9cdbdd1440cb289a477e6495173
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-14 14:07:19 +0200

    corrected misspelling from reviced to received.

--- EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
@@ -1,6 +1,7 @@
 package com.fb.dlqpattern.EAI.controller;
 
 import com.fb.dlqpattern.EAI.service.ReceivedErgebnismeldungService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class ReceivedInfoController {
-
+    @Autowired
     ReceivedErgebnismeldungService receivedErgebnismeldungService;
 
     @GetMapping("/info")
     public String info(@RequestParam(value = "name", defaultValue = "World")String name) {
-        return receivedErgebnismeldungService.reviceErgebnismeldungService(name);
+        return receivedErgebnismeldungService.receiveErgebnismeldungService(name);
     }
 }
