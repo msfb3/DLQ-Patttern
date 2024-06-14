0e78475f3baf45fff646e89e005c3f7841831b1d
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2024-06-14 08:47:55 +0200

    Update was/src/main/java/com/fb/dlqpattern/was/WasApplication.java
    Co-authored-by: Nic12345678 <162564162+Nic12345678@users.noreply.github.com>

--- was/src/main/java/com/fb/dlqpattern/was/WasApplication.java
+++ was/src/main/java/com/fb/dlqpattern/was/WasApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.fb.dlqpattern.was",
+		"de.muenchen.oss.wahllokalsystem"
+})
 public class WasApplication {
 
 	public static void main(String[] args) {
