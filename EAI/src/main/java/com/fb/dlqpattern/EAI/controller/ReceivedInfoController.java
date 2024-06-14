fedeafd27c3a896593a50a3284fc8eccf79acdab
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-06-14 14:15:24 +0200

    formelles Get in der EAI

--- EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
@@ -1,7 +1,6 @@
 package com.fb.dlqpattern.EAI.controller;
 
 import com.fb.dlqpattern.EAI.service.ReceivedErgebnismeldungService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class ReceivedInfoController {
-    @Autowired
+
     ReceivedErgebnismeldungService receivedErgebnismeldungService;
 
     @GetMapping("/info")
-    public String info(@RequestParam(value = "name", defaultValue = "World")String name) {
-        return receivedErgebnismeldungService.receiveErgebnismeldungService(name);
+    public String info(@RequestParam(value = "name", defaultValue = "Hello World")String name) {
+        return String.format("Noch nicht implementiert:", name);
     }
 }
