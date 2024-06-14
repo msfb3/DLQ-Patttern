0a489a0ccef5b69883c17e746820b1db558ac378
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-14 09:32:56 +0200

    corrected misspelling from Reviced to Received.

--- EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/controller/ReceivedInfoController.java
@@ -1,18 +1,18 @@
 package com.fb.dlqpattern.EAI.controller;
 
-import com.fb.dlqpattern.EAI.service.ReviceErgebnismeldungService;
+import com.fb.dlqpattern.EAI.service.ReceivedErgebnismeldungService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class ReviceInfoController {
+public class ReceivedInfoController {
 
-    ReviceErgebnismeldungService reviceErgebnismeldungService;
+    ReceivedErgebnismeldungService receivedErgebnismeldungService;
 
     @GetMapping("/info")
     public String info(@RequestParam(value = "name", defaultValue = "World")String name) {
-        return reviceErgebnismeldungService.reviceErgebnismeldungService(name);
+        return receivedErgebnismeldungService.reviceErgebnismeldungService(name);
     }
 }
---
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/service/ReceivedErgebnismeldungService.java
@@ -0,0 +1,12 @@
+package com.fb.dlqpattern.EAI.service;
+
+import com.fb.dlqpattern.EAI.rest.ReceivedInfoDTO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ReceivedErgebnismeldungService {
+    public void reviceErgebnismeldungService(ReceivedInfoDTO body) {
+        log.debug("Nachricht erhalten");
+    }
+
+}
