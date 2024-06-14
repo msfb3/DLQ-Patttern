5d5063562165844d42e8181c41dd6763fa964140
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-14 14:05:11 +0200

    implemented Exception Konstanten.

---
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/util/EAIExceptionKonstanten.java
@@ -0,0 +1,10 @@
+package com.fb.dlqpattern.EAI.util;
+
+public interface EAIExceptionKonstanten {
+    /**
+     * Code in der Kategorie CATEGORY_FACHLICH für Parameter unvollständig
+     */
+    String CODE_SENDINFO_PARAMETER_UNVOLLSTAENDIG = "150";
+}
+
+
