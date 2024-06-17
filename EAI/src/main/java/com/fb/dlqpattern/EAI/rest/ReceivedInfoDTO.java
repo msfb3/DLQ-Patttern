2a7552307715fa1a3303ecd33b9554daecaa0244
msfb3 <83815772+msfb3@users.noreply.github.com>
2024-06-17 09:01:13 +0200

    added missing boolean.

--- EAI/src/main/java/com/fb/dlqpattern/EAI/rest/ReceivedInfoDTO.java
+++ EAI/src/main/java/com/fb/dlqpattern/EAI/rest/ReceivedInfoDTO.java
@@ -3,7 +3,7 @@ package com.fb.dlqpattern.EAI.rest;
 import org.springframework.lang.NonNull;
 
 
-    public record ReceivedInfoDTO(@NonNull int id, @NonNull String name, @NonNull int age, @NonNull String kind) {
+    public record ReceivedInfoDTO(@NonNull int id, @NonNull String name, @NonNull int age, @NonNull String kind, @NonNull boolean isAltered) {
 
     }
 
